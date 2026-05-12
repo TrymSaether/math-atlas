5b8b47f73386d77af9cff649952217202f60096c
Trym Saether <70568409+TrymSaether@users.noreply.github.com>
2026-05-12 09:43:51 +0200

    Use cleaned enriched topology graph as app data

--- src/data.ts
+++ src/data.ts
@@ -1,5 +1,7 @@
 import raw from "./data/topology.json";
+import { cleanTopologyData } from "./lib/cleanTopologyData";
 import { TopoData, type TopoData as TopoDataType } from "./types";
 
-export const data: TopoDataType = TopoData.parse(raw);
+export const data: TopoDataType = cleanTopologyData(TopoData.parse(raw));
 export const nodeById = new Map(data.nodes.map((n) => [n.id, n]));
+export const edgeById = new Map(data.edges.map((e) => [e.id, e]));
